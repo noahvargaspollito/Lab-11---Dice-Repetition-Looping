d48309427be3f4c8edb9560fea1f482580fb8023
noahvargaspollito <noahvargaspolo73@gmail.com>
2026-07-07 11:15:52 -0700

    1st Commit: Added a simulator that rolled 100 times and printed final results

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import org.junit.runner.notification.StoppedByUserException;
+
 import java.util.Random;
 
 class Main
@@ -5,7 +7,40 @@ class Main
   public static void main(String[] args)
   {
     // Example calling the roll() method
-    System.out.println("Roll: " + roll());
+      int oneCount = 0;
+      int twoCount = 0;
+      int threeCount = 0;
+      int fourCount = 0;
+      int fiveCount = 0;
+      int sixCount = 0;
+
+
+      for (int i=0; i< 100; i++){
+            roll();
+          System.out.println(" Roll ["+ i + "]: " + roll());
+          if (roll() == 1){
+              oneCount ++;
+          }
+          else if (roll() == 2)
+              twoCount++;
+          else if (roll() == 3)
+              threeCount++;
+          else if (roll() == 4)
+              fourCount++;
+          else if (roll() == 5)
+              fiveCount++;
+          else if (roll() == 6)
+              sixCount++;
+   }
+      System.out.println("Final Roll Counts");
+      System.out.println("[1]: " + oneCount);
+      System.out.println("[2]: " + twoCount);
+      System.out.println("[3]: " + threeCount);
+      System.out.println("[4]: " + fourCount);
+      System.out.println("[5]: " + fiveCount);
+      System.out.println("[6]: " + sixCount);
+
+
   }
   
   // Random is a class we can use to generate random numbers
